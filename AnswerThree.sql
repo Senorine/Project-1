007a7d30963d60d6b237aff8d86b98619350f95d
Senorine <76982823+Senorine@users.noreply.github.com>
2021-02-10 20:32:57 -0500

    Update AnswerThree.sql

--- AnswerThree.sql
+++ AnswerThree.sql
@@ -1,25 +1,25 @@
 use testdb3;
 
-create table jan_total_views(
+create table dec_total_views(
 	page_title string,
 	total_count int	
 );
 
-insert into jan_total_views select page_title , sum(count_views * 31) from page_view_jan 
+insert into dec_total_views select page_title , sum(count_views * 31) from page_view_dec 
 where domain_code like "en%" 
 and page_title Not like "Main_Page" and page_title Not like "Special:Search" 
 and page_title not like "-"
 group by page_title ;
 
 
-create table jan_total_clickstreams(
+create table dec_total_clickstreams(
 	start_link string,
 	current_link string,
 	total_occurence int	
 );
 
 
-insert into jan_total_clickstreams select start_link, current_link, sum(occurence_count) from jan_clickstream 
+insert into dec_total_clickstreams select start_link, current_link, sum(occurence_count) from dec_clickstream 
 where link_type = "link"
 group by start_link, current_link;
 
@@ -30,10 +30,10 @@ create table frac(
 );
 
 
-insert into table frac select jan_total_clickstreams.start_link, jan_total_clickstreams.current_link,
-jan_total_clickstreams.total_occurence/jan_total_views.total_count 
-from jan_total_clickstreams 
-inner join jan_total_views on jan_total_clickstreams.start_link = jan_total_views.page_title;
+insert into table frac select dec_total_clickstreams.start_link, dec_total_clickstreams.current_link,
+dec_total_clickstreams.total_occurence/dec_total_views.total_count 
+from dec_total_clickstreams 
+inner join dec_total_views on dec_total_clickstreams.start_link = dec_total_views.page_title;
 
 --Query for finding the start of the series with highest fraction clicking internal link
 select start_link, current_link, link_frac from frac where start_link = "Hotel_California" 
